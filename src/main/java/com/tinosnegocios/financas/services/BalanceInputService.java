9e58f7d5b486772f89f7b7e94afd27b31c11fd66
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-05-15 23:34:47 -0300

    service for update balance input

--- src/main/java/com/tinosnegocios/financas/services/BalanceInputService.java
+++ src/main/java/com/tinosnegocios/financas/services/BalanceInputService.java
@@ -4,6 +4,7 @@ import com.tinosnegocios.financas.exceptions.ResourceNotFoundException;
 import com.tinosnegocios.financas.repositories.BalanceFlowRepository;
 import com.tinosnegocios.financas.repositories.BalanceInPutRepository;
 import com.tinosnegocios.financas.entities.BalanceInput;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -36,5 +37,19 @@ public class BalanceInputService {
     public void deleteById(Long id){
         balanceFlowRepository.deleteByInputId(id);
         balanceInoutRepository.deleteById(id);
+    }
+    public BalanceInput updateOne(BalanceInput balanceInput, Long id){
+        try{
+            BalanceInput inputObj = balanceInoutRepository.getReferenceById(id);
+            inputObj.setAmount(balanceInput.getAmount());
+            inputObj.setDescription(balanceInput.getDescription());
+            inputObj.setObservation(balanceInput.getObservation());
+            inputObj.setMoment(balanceInput.getMoment());
+            inputObj.setRealProfit(balanceInput.isRealProfit());
+
+            return balanceInoutRepository.save(inputObj);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 }
